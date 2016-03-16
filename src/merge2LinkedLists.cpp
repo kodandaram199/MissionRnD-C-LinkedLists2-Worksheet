bc424ff4762a582af4d3051559bf545384fa6dc6
kodandaram199 <kodandaram199@missionrnd.com>
2016-03-16 20:09:15 +0530

    Submit Commit

--- src/merge2LinkedLists.cpp
+++ src/merge2LinkedLists.cpp
@@ -19,5 +19,23 @@ struct node {
 };
 
 struct node * merge2LinkedLists(struct node *head1, struct node *head2) {
-	return NULL;
+
+
+	struct node* result = NULL;
+
+	if (head1 == NULL)
+		return head2;
+	if (head2 == NULL)
+		return head1;
+if (head1->num >= head2->num)
+{
+	result = head2;
+	result->next = merge2LinkedLists(head1, head2->next);
+}
+else
+{
+	result = head1;
+	result->next = merge2LinkedLists(head2, head1->next);
+}
+return result;
 }
